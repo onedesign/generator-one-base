f3cad870bb36ac82d842753badb84b7661ef18ba
Chris Malven <cmalven@chrismalven.com>
2016-06-01 22:34:07 -0500

    Adds more unit tests for app sub-generator.

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('pre-test', function () {
   return gulp.src('generators/**/*.js')
     .pipe(excludeGitignore())
     .pipe(istanbul({
-      includeUntested: true
+      includeUntested: false
     }))
     .pipe(istanbul.hookRequire());
 });
