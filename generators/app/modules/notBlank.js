227a64fc81ab47f6668b05dda208f6ddf732be36
Brian Hanson <brian@brianhanson.net>
2016-12-02 21:20:07 -0600

    Move functions out of prompts

---
+++ generators/app/modules/notBlank.js
@@ -0,0 +1,6 @@
+module.exports = function(input) {
+  if (input.length) {
+    return true;
+  }
+  return "Please enter a description.";
+}
--- generators/app/modules/prompts.js
+++ generators/app/modules/prompts.js
@@ -1,19 +1,6 @@
-function notBlank(input) {
-  if (input.length) {
-    return true;
-  }
-  return "Please enter a description.";
-}
+var notBlank = require('./notBlank');
+var slugify = require('./slugify');
 
-// https://gist.github.com/mathewbyrne/1280286
-function slugify(text) {
-  return text.toString().toLowerCase()
-    .replace(/\s+/g, '-')           // Replace spaces with -
-    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-    .replace(/^-+/, '')             // Trim - from start of text
-    .replace(/-+$/, '');            // Trim - from end of text
-}
 
 module.exports = [
   //
