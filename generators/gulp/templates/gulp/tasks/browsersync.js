2f36b23937faf24524e52887d50b22e72e90e8ba
Chris Malven <cmalven@chrismalven.com>
2017-02-13 20:30:37 -0600

    Adds custom styles for browsersync notifications.

--- generators/gulp/templates/gulp/tasks/browsersync.js
+++ generators/gulp/templates/gulp/tasks/browsersync.js
@@ -15,7 +15,25 @@ module.exports = gulp.task('browserSync', function() {
   var options = {
     port: 3000,
     open: false,
-    notify: false
+    notify: {
+      styles: [
+        'display: none;',
+        'padding: 9px 15px 9px;',
+        'position: fixed;',
+        'text-align: left;',
+        'font-family: monospace;',
+        'font-size: 0.7em;',
+        'line-height: 1.5;',
+        'white-space: pre;',
+        'z-index: 9999;',
+        'left: 0px;',
+        'border-top-right-radius: 4px;',
+        'bottom: 0px;',
+        'color: rgb(74, 74, 74);',
+        'background-color: rgb(20, 20, 20);',
+        'color: rgb(255, 255, 255);'
+      ]
+    }
   }
 
   if (config.useProxy) {
