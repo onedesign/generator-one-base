1d0b028447648734cd532ae3be735ff05b2741dd
Chris Malven <cmalven@chrismalven.com>
2017-08-13 16:26:03 -0500

    Moves all Imager config into .env

--- generators/craft/templates/craft/config/imager.php
+++ generators/craft/templates/craft/config/imager.php
@@ -4,19 +4,22 @@
  * Configuration file for Imager
  */
 
+// Import the environment variables file path
+$env = require(dirname(dirname(__FILE__)).'/../.env.php');
+
 return array(
   // General
-  'imagerSystemPath' => $_SERVER['DOCUMENT_ROOT'] . '/imager/',
+  'imagerSystemPath' => $env['IMAGER_SYSTEM_PATH'],
 
   // Progressive Images
   'interlace' => true,
   'allowUpscale' => false,
 
   // AWS Config
-  // 'imagerUrl' => 'https://s3.amazonaws.com/<%= projectName %>/imager/',
-  // 'awsEnabled' => true,
-  // 'awsAccessKey' => '',
-  // 'awsSecretAccessKey' => '',
-  // 'awsBucket' => '<%= projectName %>',
-  // 'awsFolder' => 'imager'
+  // 'awsEnabled' => $env['AWS_ENABLED'],
+  // 'imagerUrl' => $env['IMAGER_URL'],
+  // 'awsAccessKey' => $env['AWS_ACCESS_KEY'],
+  // 'awsSecretAccessKey' => $env['AWS_SECRET_ACCESS_KEY'],
+  // 'awsBucket' => $env['AWS_BUCKET'],
+  // 'awsFolder' => $env['AWS_FOLDER']
 );
