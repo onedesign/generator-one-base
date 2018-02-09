321d432e53fef6ca0ad86e56cb696d9b38fc1b6b
mkornatz <mkornatz@hush.com>
2018-02-08 23:29:08 -0500

    Adds environment label plugin as install option

--- generators/craft2/modules/craft_plugins.js
+++ generators/craft2/modules/craft_plugins.js
@@ -58,5 +58,12 @@ module.exports = {
     githubName: '',
     src: 'nystudio107/retour',
     checked: true
-  }
+  },
+  labelenvironment: {
+    name: 'Craft Environment Label',
+    githubName: 'craft.labelenvironment',
+    src: 'https://github.com/madebykind/craft.labelenvironment/',
+    branch: 'master',
+    checked: true
+  },
 }
