48885e92b8009e6d042f6bb8f1da43e98a8d8754
mkornatz <mkornatz@hush.com>
2018-03-12 11:10:12 -0400

    Updates name of test scope

--- __tests__/git.js
+++ __tests__/git.js
@@ -3,7 +3,7 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-describe('generator-one-git', () => {
+describe('generator-git', () => {
   describe('default', () => {
     beforeAll(() => {
       return helpers.run(path.join(__dirname, '../generators/git'));
