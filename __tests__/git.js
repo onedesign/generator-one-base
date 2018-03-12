379e04008d25b3651b76c2f8efb89b42f0a97331
mkornatz <mkornatz@hush.com>
2018-03-12 11:03:51 -0400

    Removes craft2 and craft3 specifics in git generator

--- __tests__/git.js
+++ __tests__/git.js
@@ -3,7 +3,7 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-describe('generator-one-base:git', () => {
+describe('generator-one-git', () => {
   describe('default', () => {
     beforeAll(() => {
       return helpers.run(path.join(__dirname, '../generators/git'));
@@ -16,32 +16,6 @@ describe('generator-one-base:git', () => {
         '.github/PULL_REQUEST_TEMPLATE.md',
         '.git/hooks/pre-commit'
       ]);
-    });
-  });
-
-  describe('Craft 2 ignore', () => {
-    beforeAll(() => {
-      return helpers.run(path.join(__dirname, '../generators/git'))
-        .withOptions({
-          platform: 'craft2'
-        });
-    });
-
-    it('appends the Craft 2 ignore information', () => {
-      assert.fileContent('.gitignore', '# Craft 2.X');
-    });
-  });
-
-  describe('Craft 3 ignore', () => {
-    beforeAll(() => {
-      return helpers.run(path.join(__dirname, '../generators/git'))
-        .withOptions({
-          platform: 'craft3'
-        });
-    });
-
-    it('appends the Craft 3 ignore information', () => {
-      assert.fileContent('.gitignore', '# Craft 3.X');
     });
   });
 });
