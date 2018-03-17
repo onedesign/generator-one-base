e358826ae68b94d971c518e2af7f6af7cb8391bd
Chris Malven <cmalven@chrismalven.com>
2018-03-17 15:45:31 -0500

    Improve the configuation and display of Craft 3 Environment Label plugin.

--- generators/craft3/templates/config/environment-label.php
+++ generators/craft3/templates/config/environment-label.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
-  'showLabel' => filter_var(getenv('APP_DEV_MODE'), FILTER_VALIDATE_BOOLEAN),
-  'label' => 'Testing/Staging Environment',
+  'showLabel' => filter_var(getenv('APP_ENV_LABEL'), FILTER_VALIDATE_REGEXP,  array(
+     'options' => array('regexp' => "/.*/")
+  )),
+  'labelText' => getenv('APP_ENV_LABEL'),
   'prefix' => null,
   'suffix' => null,
   'labelColor' => '#0085a1',
